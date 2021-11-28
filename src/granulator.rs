cf2ef90bd2338483f76f8f4b9806d4da6ac08738
Anton Miselaytes <5480808+miselaytes-anton@users.noreply.github.com>
2021-11-28 19:47:58 +0100

    Add Scheduler and Grain

--- src/granulator.rs
+++ src/granulator.rs
@@ -1,24 +1,35 @@
+use rand::Rng;
+
 pub const SAMPLE_RATE: usize = 41000;
 const MAX_DELAY_TIME_SECONDS: usize = 10;
 const NUM_CHANNELS: usize = 2;
+const GRAIN_AMPLITUDE: f32 = 0.95;
+const MAX_GRAINS: usize = 200;
+const GRAIN_DURATION: usize = 800;
+const SILENT_FRAME: Frame = [0.0, 0.0];
+const DELAY_FEEDBACK: f32 = 0.8;
 
 type Frame = [f32; NUM_CHANNELS];
 
 struct DelayLine {
     buffer: Vec<Frame>,
     write_index: usize,
+    delay_length: usize,
 }
 
 impl DelayLine {
-    pub fn new(max_length: usize) -> Self {
+    pub fn new(max_length: usize, delay_length: usize) -> Self {
+        assert!(delay_length <= max_length);
+
         Self {
             buffer: vec![[0.0, 0.0]; max_length],
             write_index: 0,
+            delay_length,
         }
     }
 
-    pub fn read(&self, delay_length: usize) -> Frame {
-        self.buffer[self.get_read_index(delay_length)]
+    pub fn read(&self) -> Frame {
+        self.buffer[self.get_read_index(self.delay_length)]
     }
 
     pub fn write_and_advance(&mut self, frame: Frame) {
@@ -41,7 +52,7 @@ impl DelayLine {
     }
 }
 
-#[derive(Debug)]
+#[derive(Copy, Clone)]
 struct ParabolicEnvelope {
     amplitude: f32,
     slope: f32,
@@ -82,28 +93,130 @@ impl ParabolicEnvelope {
     }
 }
 
-pub struct Granulator {
-    delay_length: usize,
-    delay_line: DelayLine,
+#[derive(Copy, Clone)]
+struct Grain {
+    is_active: bool,
+    duration: usize,
     envelope: ParabolicEnvelope,
+}
+
+impl Grain {
+    pub fn new(duration: usize) -> Grain {
+        Grain {
+            is_active: false,
+            duration,
+            envelope: ParabolicEnvelope::new(duration, GRAIN_AMPLITUDE),
+        }
+    }
+
+    pub fn process(&mut self, delay_line: &DelayLine) -> Frame {
+        if self.is_active == false {
+            return SILENT_FRAME;
+        }
+        let env = self.envelope.process();
+        let [left, right] = delay_line.read();
+
+        self.duration -= 1;
+
+        if self.duration == 0 {
+            self.is_active = false;
+            self.duration = GRAIN_DURATION;
+        }
+
+        [left * env, right * env]
+    }
+
+    pub fn activate(&mut self) {
+        self.is_active = true;
+    }
+}
+
+struct Scheduler {
+    next_onset: usize,
+    grains: [Grain; MAX_GRAINS],
+
+    delay_line: DelayLine,
+}
+
+impl Scheduler {
+    pub fn new(delay_line: DelayLine) -> Scheduler {
+        Scheduler {
+            next_onset: 0,
+            grains: [Grain::new(GRAIN_DURATION); MAX_GRAINS],
+            delay_line,
+        }
+    }
+
+    pub fn process(&mut self) -> Frame {
+        if self.next_onset == 0 {
+            self.activate_grain();
+            self.next_onset += self.next_interonset();
+        }
+        self.next_onset -= 1;
+
+        self.synthesize_active_grains()
+    }
+
+    fn synthesize_active_grains(&mut self) -> Frame {
+        let mut num_active_grains: f32 = 0.0;
+        let [mut left, mut right]: Frame = SILENT_FRAME;
+        let gain: f32 = 2.0;
+
+        for grain in self.grains.iter_mut() {
+            if grain.is_active == true {
+                let [left_grain, right_grain] = grain.process(&self.delay_line);
+                left += left_grain;
+                right += right_grain;
+                num_active_grains += 1.0;
+            }
+        }
+
+        if num_active_grains > 0.0 {
+            return [
+                left / num_active_grains * gain,
+                right / num_active_grains * gain,
+            ];
+        }
+
+        SILENT_FRAME
+    }
+
+    fn activate_grain(&mut self) {
+        for grain in self.grains.iter_mut() {
+            if grain.is_active == false {
+                grain.activate();
+                continue;
+            }
+        }
+    }
+
+    fn next_interonset(&self) -> usize {
+        let mut rng = rand::thread_rng();
+        rng.gen_range(1..10000)
+    }
+}
+
+pub struct Granulator {
+    scheduler: Scheduler,
 }
 
 impl Granulator {
     pub fn new(delay_length: usize) -> Granulator {
-        assert!(delay_length <= MAX_DELAY_TIME_SECONDS * SAMPLE_RATE);
+        let delay_line = DelayLine::new(MAX_DELAY_TIME_SECONDS * SAMPLE_RATE, delay_length);
         Granulator {
-            delay_length: delay_length,
-            delay_line: DelayLine::new(MAX_DELAY_TIME_SECONDS * SAMPLE_RATE),
-
-            envelope: ParabolicEnvelope::new(500, 0.8),
+            scheduler: Scheduler::new(delay_line),
         }
     }
     pub fn process(&mut self, frame: Frame) -> Frame {
-        self.delay_line.write_and_advance(frame);
+        let [left, right] = frame;
+        let [delayed_left, delayed_right] = self.scheduler.process();
+        let processed_frame: Frame = [
+            left - delayed_left * DELAY_FEEDBACK,
+            right - delayed_right * DELAY_FEEDBACK,
+        ];
 
-        let env = self.envelope.process();
-        let [left, right] = self.delay_line.read(self.delay_length);
+        self.scheduler.delay_line.write_and_advance(processed_frame);
 
-        [left * env, right * env]
+        processed_frame
     }
 }
